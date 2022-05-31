6e8737cda4a1ad8ecb80573470b76b15deb92e2d
nviolante25 <nviolante96@gmail.com>
2022-05-31 20:46:26 +0200

    add new shaders

--- project/shader/fragment.glsl
+++ project/shader/fragment.glsl
@@ -1,7 +1,8 @@
 #version 330
 
+in vec4 ourColor;
 out vec4 fragColor;
 
 void main() {
-    fragColor = vec4(1, 1, 1, 1);
+    fragColor = ourColor;
 }
--- project/shader/vertex.glsl
+++ project/shader/vertex.glsl
@@ -1,7 +1,17 @@
 #version 330
 
+
 layout (location = 0) in vec3 pos;
+layout (location = 1) in vec3 color;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+out vec4 ourColor;
+
 
 void main() {
-    gl_Position = vec4(pos, 1.0f);
+    gl_Position = projection * view * model * vec4(pos, 1.0f);
+    ourColor = vec4(color, 1.0f);
 }
